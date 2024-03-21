ddba5b973e11bc6dc85a1f5a882347e21c7362f5
Akkanagamma patil <83946029+Akkanagamma2@users.noreply.github.com>
2024-03-21 04:43:14 +0000

    variables snippet

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 261023;
+let accountEmail = "akkupatil@gmail.com";
+var accountPassword = "akku@1234";
+accountCity = "Gulbarga";
+
+// accountId = 12; constant variable not initialised once again
+/*var is not used commonly bcs it is used for block scope and functional scope */
+
+accountEmail = "akku@gmail.com"
+accountPassword = "1234@akku"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
