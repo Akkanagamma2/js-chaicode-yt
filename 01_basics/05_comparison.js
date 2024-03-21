1cba80e99c85a6e3a86015805ea65818ea4c3109
Akkanagamma patil <83946029+Akkanagamma2@users.noreply.github.com>
2024-03-21 05:58:24 +0000

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,21 @@
+// console.log(3 > 2);
+// console.log("aaku" > "abhi");
+// console.log(1 == true);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+// console.log(0 >= null); //true
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+
+// === strict comparison (datatype validation)
+
+console.log("2" === 2);
+console.log(0 === false);
+
+
+
