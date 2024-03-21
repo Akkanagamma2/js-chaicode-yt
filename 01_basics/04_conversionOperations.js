b6f845f7e8fbd67b2215cef5d3fccee317d2cead
Akkanagamma patil <83946029+Akkanagamma2@users.noreply.github.com>
2024-03-21 05:44:02 +0000

    operations

---
+++ 01_basics/04_conversionOperations.js
@@ -0,0 +1,23 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+
+// console.log("1" + 2);
+// console.log(2 + "2");
+// console.log("1" + "2" + 5);
+// console.log(2 + 2 + "1");
+// console.log("2" + 2 + "1");
+
+console.log(true);
+console.log(+true);
+console.log(+false);
+
+//prefix and postfix
+let counter = 100;
+let postfixCounter = ++counter;
+let prefixCounter = counter++;
+console.log(postfixCounter);
+console.log(prefixCounter);
+console.log(counter);
