6207463680cd6eebe2e280fd69ffc976d27748c5
Akkanagamma patil <83946029+Akkanagamma2@users.noreply.github.com>
2024-03-21 11:19:43 +0000

    String Changes

---
+++ 01_basics/06_string.js
@@ -0,0 +1,16 @@
+let firstName = "Akkanagamma";
+let lastName = "Patil";
+let jobRole = "Software Engineer";
+
+let detail = `Hey ${firstName.toUpperCase()} ${lastName.toUpperCase()} you are selected for the role of ${jobRole} in our company`;
+
+// console.log(detail);
+
+const name = new String('Akkanagamma patil');
+console.log(name);
+console.log(name.toUpperCase());
+
+console.log(name.split(' '));
+console.log(typeof(name.split(' ')));
+
+console.log(firstName.slice(-8,5));
